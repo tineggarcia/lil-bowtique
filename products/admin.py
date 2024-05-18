bd498bab440f7471bb67fadec53f95eb400588aa
Christine Garcia <tineggarcia@gmail.com>
2024-05-18 13:32:02 +0000

    Register the models to the admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Category, User, Order, Order_item
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(Order_item)
